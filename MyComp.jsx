210597e895f1cef17e3cf6d1651a5a1c66a8361c
shifa0722 <pshifa304@gmail.com>
2026-09-11 23:28:28 +0530

    Add files via upload

--- MyComp.jsx
+++ MyComp.jsx
@@ -1,11 +1,20 @@
-  export default function MyComp (){
-    return(
-    <>
-    <h2>My Component</h2>
-    <p>lorem ipsum dolor sit amet</p>
-    <p>this is my component</p>
-    </>
+import { useState } from "react";
 
+export default function MyComp() {
+    const [age, setAge] = useState(290);
+
+    const myFun = () => {
+        setAge(29)
+
+    };
+
+    return (
+        <>
+            <h2>
+                my age is {age}
+            </h2>
+            <button onClick={myFun}>click me</button>
+
+        </>
     );
-
 }
